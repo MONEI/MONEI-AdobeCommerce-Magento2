65477d24ce5cb566465ce7c12a71fd008638243e
Monei Magento <moneimagento@gmail.com>
2024-04-26 13:53:59 +0200

    Show available methods by account monei. Validate agreements for Bizum and Google,Apple Pay

--- Plugin/ShippingInformationManagement.php
+++ Plugin/ShippingInformationManagement.php
@@ -14,24 +14,30 @@ use Magento\Checkout\Api\Data\ShippingInformationInterface;
 use Magento\Checkout\Model\ShippingInformationManagement as MagentoShippingInformationManagement;
 use Monei\MoneiPayment\Api\Config\MoneiPaymentModuleConfigInterface;
 use Monei\MoneiPayment\Model\Payment\Monei;
+use Monei\MoneiPayment\Service\Shared\GetAvailableMoneiPaymentMethodsByCountry;
+use Monei\MoneiPayment\Service\Shared\GetMoneiPaymentCodesByMagentoPaymentCode;
 
 class ShippingInformationManagement
 {
-    private const PAYMENT_METHODS_MONEI = [
-        Monei::CODE,
-        Monei::CARD_CODE,
-        Monei::CC_VAULT_CODE,
-        Monei::BIZUM_CODE,
-        Monei::GOOGLE_APPLE_CODE
-    ];
 
     private MoneiPaymentModuleConfigInterface $moneiPaymentModuleConfig;
+    private GetAvailableMoneiPaymentMethodsByCountry $getAvailableMoneiPaymentMethodsByCountry;
+    private GetMoneiPaymentCodesByMagentoPaymentCode $getMoneiPaymentCodesByMagentoPaymentCode;
 
-    public function __construct(MoneiPaymentModuleConfigInterface $moneiPaymentModuleConfig)
+    public function __construct(
+        MoneiPaymentModuleConfigInterface        $moneiPaymentModuleConfig,
+        GetAvailableMoneiPaymentMethodsByCountry $getAvailableMoneiPaymentMethodsByCountry,
+        GetMoneiPaymentCodesByMagentoPaymentCode $getMoneiPaymentCodesByMagentoPaymentCode
+    )
     {
         $this->moneiPaymentModuleConfig = $moneiPaymentModuleConfig;
+        $this->getAvailableMoneiPaymentMethodsByCountry = $getAvailableMoneiPaymentMethodsByCountry;
+        $this->getMoneiPaymentCodesByMagentoPaymentCode = $getMoneiPaymentCodesByMagentoPaymentCode;
     }
 
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function aroundSaveAddressInformation(
         MagentoShippingInformationManagement $subject,
         callable                             $proceed,
@@ -39,28 +45,63 @@ class ShippingInformationManagement
         ShippingInformationInterface         $addressInformation
     )
     {
-        $hasChange = false;
-        /** @var PaymentDetailsInterface $paymentDetails */
         $paymentDetails = $proceed($cartId, $addressInformation);
-        $paymentMethods = $paymentDetails->getPaymentMethods();
+
+        return $this->filterPaymentMethods($paymentDetails, $addressInformation);
+    }
+
+    private function filterPaymentMethods(
+        PaymentDetailsInterface $paymentDetails,
+        ShippingInformationInterface $addressInformation
+    ): PaymentDetailsInterface
+    {
         $accountId = $this->moneiPaymentModuleConfig->getAccountId();
         $apiKey = $this->moneiPaymentModuleConfig->getApiKey();
 
-        if (!empty($accountId) && !empty($apiKey)) {
-            return $paymentDetails;
+        if (empty($accountId) || empty($apiKey)) {
+            return $this->disableMoneiPaymentMethods($paymentDetails);
         }
 
-        foreach ($paymentMethods as $index => $paymentMethod) {
-            if (in_array($paymentMethod->getCode(), self::PAYMENT_METHODS_MONEI, true)) {
-                unset($paymentMethods[$index]);
-                $hasChange = true;
+        return $this->disablePaymentMethodsByShippingAddress($paymentDetails, $addressInformation);
+    }
+
+    private function disablePaymentMethodsByShippingAddress(PaymentDetailsInterface $paymentDetails, ShippingInformationInterface $addressInformation): PaymentDetailsInterface
+    {
+        $paymentMethods = $paymentDetails->getPaymentMethods();
+        $shippingAddress = $addressInformation->getShippingAddress();
+        $availableMoneiPaymentMethodsByCountry = $this->getAvailableMoneiPaymentMethodsByCountry->execute(
+            $shippingAddress->getCountryId()
+        );
+
+        $filteredPaymentMethods = [];
+        foreach ($paymentMethods as $paymentMethod) {
+            $moneiPaymentCodes = $this->getMoneiPaymentCodesByMagentoPaymentCode->execute($paymentMethod->getCode());
+            if (!$moneiPaymentCodes || $this->isPaymentMethodAllowed($moneiPaymentCodes, $availableMoneiPaymentMethodsByCountry)) {
+                $filteredPaymentMethods[] = $paymentMethod;
             }
         }
 
-        if ($hasChange) {
-            $paymentDetails->setPaymentMethods($paymentMethods);
-        }
+        $paymentDetails->setPaymentMethods($filteredPaymentMethods);
+        return $paymentDetails;
+    }
 
+    private function isPaymentMethodAllowed(array $moneiPaymentCodes, array $availableMoneiPaymentMethodsByCountry): bool
+    {
+        foreach ($moneiPaymentCodes as $moneiPaymentCode) {
+            if (in_array($moneiPaymentCode, $availableMoneiPaymentMethodsByCountry, true)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function disableMoneiPaymentMethods(PaymentDetailsInterface $paymentDetails): PaymentDetailsInterface
+    {
+        $paymentMethods = $paymentDetails->getPaymentMethods();
+        $filteredPaymentMethods = array_filter($paymentMethods, function ($paymentMethod) {
+            return !in_array($paymentMethod->getCode(), Monei::PAYMENT_METHODS_MONEI, true);
+        });
+        $paymentDetails->setPaymentMethods($filteredPaymentMethods);
         return $paymentDetails;
     }
 }
