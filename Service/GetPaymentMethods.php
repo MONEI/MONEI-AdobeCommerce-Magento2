900fbd19e53130bd70b8a79591db5c44775de8dc
Monei Magento <moneimagento@gmail.com>
2024-12-19 20:24:10 +0100

    Fix problems in checkout without data in the config

--- Service/GetPaymentMethods.php
+++ Service/GetPaymentMethods.php
@@ -59,25 +59,29 @@ class GetPaymentMethods extends AbstractService implements GetPaymentMethodsInte
         $storeId = (int) $this->storeManager->getStore()->getId();
         $accountId = $this->registryAccountId->get() ?? $this->moduleConfig->getAccountId($storeId);
 
-        $client = $this->createClient();
+        if ($accountId) {
+            $client = $this->createClient();
 
-        $this->logger->debug('------------------ START GET PAYMENT METHODS REQUEST -----------------');
-        $this->logger->debug('Account id = ' . $accountId);
-        $this->logger->debug('------------------- END GET PAYMENT METHODS REQUEST ------------------');
-        $this->logger->debug('');
+            $this->logger->debug('------------------ START GET PAYMENT METHODS REQUEST -----------------');
+            $this->logger->debug('Account id = ' . $accountId);
+            $this->logger->debug('------------------- END GET PAYMENT METHODS REQUEST ------------------');
+            $this->logger->debug('');
 
-        $response = $client->get(
-            self::METHOD . '?accountId=' . $accountId . '&' . time(),
-            [
-                'headers' => $this->getHeaders(),
-            ]
-        );
+            $response = $client->get(
+                self::METHOD . '?accountId=' . $accountId . '&' . time(),
+                [
+                    'headers' => $this->getHeaders(),
+                ]
+            );
 
-        $this->logger->debug('----------------- START GET PAYMENT METHODS RESPONSE -----------------');
-        $this->logger->debug((string) $response->getBody());
-        $this->logger->debug('------------------ END GET PAYMENT METHODS RESPONSE ------------------');
-        $this->logger->debug('');
+            $this->logger->debug('----------------- START GET PAYMENT METHODS RESPONSE -----------------');
+            $this->logger->debug((string) $response->getBody());
+            $this->logger->debug('------------------ END GET PAYMENT METHODS RESPONSE ------------------');
+            $this->logger->debug('');
 
-        return $this->serializer->unserialize($response->getBody());
+            return $this->serializer->unserialize($response->getBody());
+        }
+
+        return [];
     }
 }
