f6319227b9b6b0f845038f31e7dba6789327cfa6
Dmitriy <dn@monei.com>
2025-03-14 17:07:36 +0100

    feat: Implement JSON validation for Monei payment method configurations
    - Add a new JsonValidator backend model to validate JSON format for payment method configurations in system.xml.
    - Update JSON style fields for card, bizum, and google/apple components to ensure valid JSON input, enhancing user experience and preventing configuration errors.
    - Include detailed error messages in English and Spanish for invalid JSON formats in translation files, improving clarity for users.

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -5,7 +5,25 @@
 var config = {
   map: {
     '*': {
-      'Magento_Sales/order/view/post-wrapper': 'Monei_MoneiPayment/js/post-wrapper'
+      'Magento_Sales/order/view/post-wrapper': 'Monei_MoneiPayment/js/post-wrapper',
+      moneiJsonValidator: 'Monei_MoneiPayment/js/json-validator',
+      moneiJsonFormatter: 'Monei_MoneiPayment/js/json-formatter'
+    }
+  },
+  shim: {
+    'Monei_MoneiPayment/js/json-formatter': {
+      deps: ['jquery', 'jquery/ui']
+    },
+    'Monei_MoneiPayment/js/json-validator': {
+      deps: ['jquery', 'jquery/validate']
+    }
+  },
+  deps: ['moneiJsonValidator', 'moneiJsonFormatter'],
+  config: {
+    mixins: {
+      'mage/validation': {
+        'Monei_MoneiPayment/js/validation-mixin': true
+      }
     }
   }
 };
