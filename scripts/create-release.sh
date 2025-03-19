b488f70bde831d0c9e3dc6a34490c3c0b3bd00b1
Dmitriy <dn@monei.com>
2025-03-19 15:16:18 +0100

    chore: Add script for creating release zip and update .gitignore
    - Introduced a new script, create-release.sh, to automate the creation of a zip archive while excluding specified files and directories.
    - Updated .gitignore to include the scripts directory to prevent unnecessary files from being tracked.

---
+++ scripts/create-release.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Set the output filename
+OUTPUT_FILE="monei-module-monei-payment.zip"
+
+# Create a temporary exclusions file
+TMP_EXCLUDE="/tmp/monei-exclude.txt"
+
+# Create list of exclusions
+cat >"$TMP_EXCLUDE" <<EOL
+*.git*
+*.github*
+*Test*
+.husky/*
+.yarn/*
+.vscode/*
+.cursor/*
+vendor/*
+composer.lock
+package.json
+yarn.lock
+.yarnrc.yml
+.nvmrc
+.prettierrc
+.prettierignore
+prettyphp.json
+pretty-php.phar*
+.cursorignore
+release-it.json
+commitlint.config.js
+bin
+scripts/*
+EOL
+
+# Remove old zip if it exists
+if [ -f "$OUTPUT_FILE" ]; then
+    rm "$OUTPUT_FILE"
+fi
+
+# Create zip archive excluding specified patterns
+zip -r "$OUTPUT_FILE" . -x@"$TMP_EXCLUDE"
+
+# Clean up temporary file
+rm "$TMP_EXCLUDE"
+
+echo "Created archive: $OUTPUT_FILE"
